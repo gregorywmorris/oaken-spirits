7d6d35ad6d7613765d5aa0a32f51d081f1ff8004
gregorywmorris <gwm1980@gmail.com>
2024-03-21 19:12:13 -0400

    data now flowing to msql topic

--- src/oaken-services/app/mysql/init.sql
+++ src/oaken-services/app/mysql/init.sql
@@ -1,76 +1,78 @@
 CREATE DATABASE IF NOT EXISTS oaken;
 USE oaken;
-DROP TABLE IF EXISTS customer;
+
+DROP TABLE IF EXISTS salesLedger;
 DROP TABLE IF EXISTS sales;
-DROP TABLE IF EXISTS vendor;
+DROP TABLE IF EXISTS customer;
 DROP TABLE IF EXISTS product;
-DROP TABLE IF EXISTS ledger;
+DROP TABLE IF EXISTS vendor;
+DROP TABLE IF EXISTS category;
 DROP TABLE IF EXISTS county;
 
 CREATE TABLE county (
-    CountyNumber int not null,
-    County varchar(255) not null,
-    PRIMARY KEY(county)
+    CountyNumber INT NOT NULL,
+    CountyName VARCHAR(255) NOT NULL,
+    PRIMARY KEY (CountyNumber)
 );
 
 CREATE TABLE customer (
-    StoreNumber  int not null,
-    StoreName varchar(255) not null,
-    Address varchar(255) not null,
-    City varchar(255) not null,
-    State varchar(255) not null,
-    CountyNumber int not null
-    ZipCode int not null,
-    PRIMARY KEY(StoreNumber),
-    FOREIGN KEY (CountyNumber) REFERENCES county(cCountyNumber)
+    StoreNumber INT NOT NULL,
+    StoreName VARCHAR(255) NOT NULL,
+    Address VARCHAR(255) NOT NULL,
+    City VARCHAR(255) NOT NULL,
+    State VARCHAR(255) NOT NULL,
+    CountyNumber INT,
+    ZipCode INT NOT NULL,
+    PRIMARY KEY (StoreNumber),
+    FOREIGN KEY (CountyNumber) REFERENCES county(CountyNumber)
 );
 
 CREATE TABLE vendor (
-    VendorNumber int not null,
-    VendorName varchar(255) not null,
-    PRIMARY KEY(StoreNumber)
+    VendorNumber INT NOT NULL,
+    VendorName VARCHAR(255) NOT NULL,
+    PRIMARY KEY (VendorNumber)
 );
 
 CREATE TABLE category (
-    Category int not null,
-    CategoryName varchar(255) not null,
-    PRIMARY KEY(Category)
+    CategoryNumber INT NOT NULL,
+    CategoryName VARCHAR(255) NOT NULL,
+    PRIMARY KEY (CategoryNumber)
 );
 
 CREATE TABLE product (
-    ItemNumber int not null,
-    Category int not null,
-    ItemDescription varchar(255) not null,
-    BottleVolume(ml)  int not null,
-    Pack  int not null,
-    BottleCost int not null,
-    BottleRetail int not null,
-    PRIMARY KEY(ItemNumber),
-    FOREIGN KEY (Category) REFERENCES category(Category)
+    ItemNumber INT NOT NULL,
+    CategoryNumber INT,
+    ItemDescription VARCHAR(255) NOT NULL,
+    BottleVolumeML INT NOT NULL,
+    Pack INT NOT NULL,
+    BottleCost INT NOT NULL,
+    BottleRetail INT NOT NULL,
+    PRIMARY KEY (ItemNumber),
+    FOREIGN KEY (CategoryNumber) REFERENCES category(CategoryNumber)
 );
 
 CREATE TABLE sales (
-    Invoice varchar(255) not null,
-    StoreNumber  int not null,
-    VendorNumber int not null,
-    SalesDate datetime not null,
-    Sale(Dollars) float64 not null,
-    ItemNumber int not null,
-    BottlesSold int not null,
-    VolumeSold(Liters) float64 not null,
-    ShippingDate datetime,
-    ShippingCost float64,
-    PRIMARY KEY(invoice),
+    Invoice VARCHAR(255) NOT NULL,
+    StoreNumber INT NOT NULL,
+    VendorNumber INT NOT NULL,
+    SalesDate DATETIME NOT NULL,
+    SaleDollars FLOAT NOT NULL,
+    ItemNumber INT NOT NULL,
+    BottlesSold INT NOT NULL,
+    VolumeSoldLiters FLOAT NOT NULL,
+    ShippingDate DATETIME,
+    ShippingCost FLOAT,
+    PRIMARY KEY (Invoice),
     FOREIGN KEY (StoreNumber) REFERENCES customer(StoreNumber),
     FOREIGN KEY (ItemNumber) REFERENCES product(ItemNumber),
     FOREIGN KEY (VendorNumber) REFERENCES vendor(VendorNumber)
 );
 
 CREATE TABLE salesLedger (
-    Invoice varchar(255) not null,
-    Credit int,
-    Debit int,
-    Note varchar(255),
-    FOREIGN KEY (Invoice) REFERENCES sales(Invoice),
-    PRIMARY KEY(Invoice)
+    Invoice VARCHAR(255) NOT NULL,
+    Credit INT,
+    Debit INT,
+    Note VARCHAR(255),
+    PRIMARY KEY (Invoice),
+    FOREIGN KEY (Invoice) REFERENCES sales(Invoice)
 );
