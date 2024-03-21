1d26ac8000feebd5010f3e4c51d03fddbfd9362c
gregorywmorris <gwm1980@gmail.com>
2024-03-21 11:27:06 -0400

    .env updates

--- src/oaken-services/kafka-topics.sh
+++
@@ -1,4 +0,0 @@
-# Create Kafka topics
-kafka-topics.sh --create --topic mysql --bootstrap-server kafka1:9092 --replication-factor 1 --partitions 1
-kafka-topics.sh --create --topic invoices --bootstrap-server kafka1:9092 --replication-factor 1 --partitions 1
-kafka-topics.sh --create --topic shipping --bootstrap-server kafka1:9092 --replication-factor 1 --partitions 1
