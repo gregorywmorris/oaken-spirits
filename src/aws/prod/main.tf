d358fc6dc9a5b7ad9640c0ae4cf83ac096cdeb68
gregorywmorris <gwm1980@gmail.com>
2024-03-25 15:29:03 -0400

    symbolic link, starting terraform

---
+++ src/aws/prod/main.tf
@@ -0,0 +1,29 @@
+# Oaken Spirits
+/*
+
+*/
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.environment == "development" ? "t2.micro" : "t2.small"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
