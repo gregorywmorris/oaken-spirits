b076f980db11eeb350a053ef9d7b872f9644fc42
gregorywmorris <gwm1980@gmail.com>
2024-03-28 06:01:25 -0400

    terraform

--- src/production/aws/main.tf
+++ src/production/aws/main.tf
@@ -1,5 +1,7 @@
 # Oaken Spirits
 /*
+Variable set in variables.tf
+
 EC2 Instances
 1. Database
   - MySQL
@@ -20,17 +22,6 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  region                   = "us-east-1"
-  profile                  = "eb-cli"
-}
-
-variable "environment" {
-  type = string
-  default = test
-}
-
 data "aws_ami" "ubuntu" {
   most_recent   = true
   filter {
@@ -46,33 +37,65 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "kafka" {
-  ami           = data.aws_ami.ubuntu.id
-  # instance_type = var.environment == "development" ? "t2.micro" : "t2.small"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "oaken"
-  }
+# Allow inbound SSH for EC2 instances
+resource "aws_security_group_rule" "allow_ssh_in" {
+  description       = "Allow SSH"
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["98.25.41.64/32"]
+  security_group_id = aws_security_group.default.id
 }
+
+# Allow all outbound traffic
+resource "aws_security_group_rule" "allow_all_out" {
+  description       = "Allow outbound traffic"
+  type              = "egress"
+  from_port         = "0"
+  to_port           = "0"
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.default.id
+}
+
+# Allow inbound MySQL connections
+resource "aws_security_group_rule" "allow_mysql_in" {
+  description              = "Allow inbound MySQL connections"
+  type                     = "ingress"
+  from_port                = "3306"
+  to_port                  = "3306"
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.default.id
+  security_group_id        = aws_security_group.rds_sg.id
+}
+
+
+# EC2 instances
 
 resource "aws_instance" "database" {
   ami           = data.aws_ami.ubuntu.id
-  # instance_type = var.environment == "development" ? "t2.micro" : "t2.small"
-  instance_type = "t3.micro"
+  instance_type = var.environment == "development" ? "t2.micro" : "t2.small"
 
   tags = {
-    Name = "oaken"
+    Name = "oaken-database"
+  }
+}
+
+resource "aws_instance" "kafka" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.environment == "development" ? "t2.micro" : "t2.small"
+
+  tags = {
+    Name = "oaken-kafka"
   }
 }
 
 resource "aws_instance" "api" {
   ami           = data.aws_ami.ubuntu.id
-  # instance_type = var.environment == "development" ? "t2.micro" : "t2.small"
-  instance_type = "t2.micro"
-
+  instance_type = var.environment == "development" ? "t2.micro" : "t2.small"
   tags = {
-    Name = "oaken"
+    Name = "oaken-mysql-api"
   }
 
   provisioner "file" {
@@ -83,11 +106,9 @@ resource "aws_instance" "api" {
 
 resource "aws_instance" "shipping" {
   ami           = data.aws_ami.ubuntu.id
-  # instance_type = var.environment == "development" ? "t2.micro" : "t2.small"
-  instance_type = "t2.micro"
-
+  instance_type = var.environment == "development" ? "t2.micro" : "t2.small"
   tags = {
-    Name = "oaken"
+    Name = "oaken-shipping"
   }
 
     provisioner "file" {
@@ -98,11 +119,9 @@ resource "aws_instance" "shipping" {
 
 resource "aws_instance" "accounting" {
   ami           = data.aws_ami.ubuntu.id
-  # instance_type = var.environment == "development" ? "t2.micro" : "t2.small"
-  instance_type = "t2.micro"
-
+  instance_type = var.environment == "development" ? "t2.micro" : "t2.small"
   tags = {
-    Name = "oaken"
+    Name = "oaken-accounting"
   }
 
     provisioner "file" {
--- src/production/aws/provider.tf
+++ src/production/aws/provider.tf
@@ -1,2 +1,6 @@
-# Providers
+# Configure the AWS Provider
+provider "aws" {
+  region                   = "us-east-1"
+  profile                  = "eb-cli" # The profile within AWS CLI config file
+}
 
