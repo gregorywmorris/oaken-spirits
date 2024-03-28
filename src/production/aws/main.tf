5659953fbdef915ce321a3a3449930054a4daf4e
gregorywmorris <gwm1980@gmail.com>
2024-03-28 06:49:47 -0400

    terraform

--- src/production/aws/main.tf
+++ src/production/aws/main.tf
@@ -76,6 +76,28 @@ resource "aws_security_group_rule" "allow_mysql_in" {
 resource "aws_instance" "database" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.environment == "development" ? "t2.micro" : "t2.small"
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y mysql-server
+
+              # Start the MySQL service
+              systemctl start mysql
+
+              # Enable MySQL service to start on boot
+              systemctl enable mysql
+
+              # Execute MySQL commands to create user and grant privileges
+              mysql <<EOF_MYSQL
+              CREATE DATABASE IF NOT EXISTS oaken;
+              CREATE USER 'mysql'@'localhost' IDENTIFIED BY 'mysql';
+              GRANT ALL PRIVILEGES ON *.* TO 'mysql'@'localhost';
+              FLUSH PRIVILEGES;
+              EOF_MYSQL
+
+              # Clean up apt cache
+              apt-get clean
+              EOF
 
   tags = {
     Name = "oaken-database"
@@ -85,15 +107,38 @@ resource "aws_instance" "database" {
 resource "aws_instance" "kafka" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.environment == "development" ? "t2.micro" : "t2.small"
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install net-tools
+              apt-get install -y default-jre
+              apt-get install -y zookeeperd
+
+              # Download and extract Kafka binary package
+              wget https://downloads.apache.org/kafka/3.7.0/kafka_2.13-3.7.0.tgz
+              tar -xzf kafka_2.13-3.7.0.tgz
+
+              # Clean up
+              apt-get clean
+              EOF
 
   tags = {
     Name = "oaken-kafka"
   }
 }
 
+
 resource "aws_instance" "api" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.environment == "development" ? "t2.micro" : "t2.small"
+  user_data = <<-EOF
+              #!/bin/bash
+                apt-get update
+                apt-get install -y pkg-config
+                pip install kafka-python mysql-connector-python
+                mkdir -p /app
+                apt-get clean
+              EOF
   tags = {
     Name = "oaken-mysql-api"
   }
@@ -107,6 +152,14 @@ resource "aws_instance" "api" {
 resource "aws_instance" "shipping" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.environment == "development" ? "t2.micro" : "t2.small"
+  user_data = <<-EOF
+              #!/bin/bash
+                apt-get update
+                apt-get install -y pkg-config
+                pip install kafka-python mysql-connector-python
+                mkdir -p /app
+                apt-get clean
+              EOF
   tags = {
     Name = "oaken-shipping"
   }
@@ -120,6 +173,14 @@ resource "aws_instance" "shipping" {
 resource "aws_instance" "accounting" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.environment == "development" ? "t2.micro" : "t2.small"
+  user_data = <<-EOF
+              #!/bin/bash
+                apt-get update
+                apt-get install -y pkg-config
+                pip install kafka-python mysql-connector-python
+                mkdir -p /app
+                apt-get clean
+              EOF
   tags = {
     Name = "oaken-accounting"
   }
