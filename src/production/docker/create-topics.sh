40cb3780f912aed41a2a08541b6775d875af9f4b
gregorywmorris <gwm1980@gmail.com>
2024-04-01 07:05:28 -0400

    docker workflow is working as desiged

--- src/production/docker/create-topics.sh
+++ src/production/docker/create-topics.sh
@@ -4,6 +4,6 @@
 sleep 10
 
 # Create topics using Kafka topic creation command
-kafka-topics --create --zookeeper zoo1:2181 --replication-factor 3 --partitions 3 --topic mysql
-kafka-topics --create --zookeeper zoo1:2181 --replication-factor 3 --partitions 3 --topic invoices
-kafka-topics --create --zookeeper zoo1:2181 --replication-factor 3 --partitions 3 --topic shipping
+kafka-topics --create --bootstrap-server kafka1:19092,kafka2:19093,kafka3:19094,kafka1:9092,kafka2:9093,kafka3:9094 --replication-factor 3 --partitions 3 --topic mysql
+kafka-topics --create --bootstrap-server kafka1:19092,kafka2:19093,kafka3:19094,kafka1:9092,kafka2:9093,kafka3:9094 --replication-factor 3 --partitions 3 --topic invoices
+kafka-topics --create --bootstrap-server kafka1:19092,kafka2:19093,kafka3:19094,kafka1:9092,kafka2:9093,kafka3:9094 --replication-factor 3 --partitions 3 --topic shipping
