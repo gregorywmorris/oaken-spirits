5aa1839f6edabcac62395122dfdd0e98fff7fb00
gregorywmorris <gwm1980@gmail.com>
2024-05-02 08:21:26 -0400

    remove mysql and replaced with postgres

--- documentation/SQL_STYLE_GUIDE.md
+++ documentation/SQL_STYLE_GUIDE.md
@@ -1,22 +1,27 @@
 # SQL Style Guide
 
 1. Table names are plural
+1. Table names are lowercase and underscore word separators
+1. Column names are singular
 1. Column names are lowercase and underscore word separators
 1. Date/time columns end with _@
 1. Table Alias fully qualified names
 1. Analytics table named for intended use
-1. SQL action all upper case
-1. Indent all code related to first SQL action
-1. New line after SQL action
-1. Single indentation for SQL actions
+1. SQL statements all upper case
+1. Indent all code related to first SQL statements
+1. New line after SQL statements
+1. Single indentation for SQL statements
 1. Double indention for all others
 1. Trailing commas
-1. Alternative action
+1. Alternative statements
     * IF ELSE for single alternative
     * CASE WHEN for multiple alternatives
-1. Boolean values True and False
+1. Boolean values 1 for True and 0 for False
 1. Alias aggregate date with descriptive name
-1. Semicolon at the end of the last line
+1. INSERT INTO statements are single line
+1. All exceptions must have documented reasoning
+    * Prioritize readability for naming
+    * SQL statement exceptions allowed for performance justification
 
 **Example:**
 
@@ -28,4 +33,6 @@ SELECT
         domesticated_pets
     LEFT JOIN
         zoo on zoo.cats = domesticated_pets.cats;
+
+INSERT INTO employees VALUES (10000,'first','last',100,10000,1);
 ```
--- src/production/docker/app/mysql-startup/init.sql
+++ src/production/docker/app/mysql-startup/init.sql
@@ -1,67 +1,132 @@
-USE oaken;
+CREATE DATABASE oaken;
 
 CREATE TABLE customers (
-    StoreNumber MEDIUMINT NOT NULL,
+    store_id INTEGER NOT NULL,
     StoreName VARCHAR(255) NOT NULL,
-    Address VARCHAR(255) NOT NULL,
-    City VARCHAR(255) NOT NULL,
-    CountyName VARCHAR(255) NOT NULL,
-    State VARCHAR(255) NOT NULL,
-    ZipCode MEDIUMINT NOT NULL,
-    PRIMARY KEY (StoreNumber)
+    street_address VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    county VARCHAR(255) NOT NULL,
+    us_state VARCHAR(255) NOT NULL,
+    zip_code INTEGER NOT NULL,
+    PRIMARY KEY (store_id)
 );
 
 CREATE TABLE vendors (
-    VendorNumber MEDIUMINT NOT NULL,
-    VendorName VARCHAR(255) NOT NULL,
-    PRIMARY KEY (VendorNumber)
+    vendor_id INTEGER NOT NULL,
+    vendor_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (vendor_id)
 );
 
 CREATE TABLE categories (
-    CategoryNumber MEDIUMINT NOT NULL,
-    CategoryName VARCHAR(255) NOT NULL,
-    PRIMARY KEY (CategoryNumber)
+    category_id INTEGER NOT NULL,
+    category_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (category_id)
 );
 
 CREATE TABLE products (
-    ItemNumber MEDIUMINT NOT NULL,
-    CategoryNumber MEDIUMINT,
-    ItemDescription VARCHAR(255) NOT NULL,
-    BottleVolumeML INT NOT NULL,
-    Pack MEDIUMINT NOT NULL,
-    BottleCost DECIMAL(11,2) NOT NULL,
-    BottleRetail DECIMAL(11,2) NOT NULL,
-    PRIMARY KEY (ItemNumber),
-    FOREIGN KEY (CategoryNumber) REFERENCES categories(CategoryNumber)
+    item_id INTEGER NOT NULL,
+    category_id INTEGER,
+    item_description VARCHAR(255) NOT NULL,
+    bottle_volume_ml INTEGER NOT NULL,
+    pack INTEGER NOT NULL,
+    bottle_cost DECIMAL(11,2) NOT NULL,
+    bottle_retail DECIMAL(11,2) NOT NULL,
+    PRIMARY KEY (item_id),
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
+
+CREATE TABLE positions (
+    title_id INTEGER NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    PRIMARY KEY (title_id)
+);
+
+CREATE TABLE employees (
+    employee_id INTEGER NOT NULL,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    title INTEGER,
+    manager_id INTEGER,
+    active BOOLEAN NOT NULL,
+    PRIMARY KEY (employee_id),
+    FOREIGN KEY (title) REFERENCES positions(title_id),
+    FOREIGN KEY (manager_id) REFERENCES employees(employee_id)
 );
 
 CREATE TABLE sales (
-    Invoice VARCHAR(255) NOT NULL,
-    StoreNumber MEDIUMINT NOT NULL,
-    VendorNumber MEDIUMINT NOT NULL,
-    SaleDate DATE NOT NULL,
-    SaleDollars DECIMAL(11,2) NOT NULL,
-    ItemNumber MEDIUMINT NOT NULL,
-    BottlesSold MEDIUMINT NOT NULL,
-    VolumeSoldLiters DECIMAL(11,2)  NOT NULL,
-    ShippingDate DATE,
-    ShippingCost DECIMAL(11,2),
-    PRIMARY KEY (Invoice),
-    FOREIGN KEY (StoreNumber) REFERENCES customers(StoreNumber),
-    FOREIGN KEY (ItemNumber) REFERENCES products(ItemNumber),
-    FOREIGN KEY (VendorNumber) REFERENCES vendors(VendorNumber)
+    invoice VARCHAR(255) NOT NULL,
+    store_id INTEGER NOT NULL,
+    vendor_id INTEGER NOT NULL,
+    employee_id INTEGER NOT NULL,
+    sale_date DATE NOT NULL,
+    sale_amount DECIMAL(11,2) NOT NULL,
+    item_id INTEGER NOT NULL,
+    bottle_count INTEGER NOT NULL,
+    volume_liter DECIMAL(11,2)  NOT NULL,
+    shipping_date DATE,
+    shipping_cost DECIMAL(11,2),
+    PRIMARY KEY (invoice),
+    FOREIGN KEY (store_id) REFERENCES customers(store_id),
+    FOREIGN KEY (item_id) REFERENCES products(item_id),
+    FOREIGN KEY (vendor_id) REFERENCES vendors(vendor_id),
+    FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
 );
 
-CREATE TABLE salesLedger (
-    Invoice VARCHAR(255) NOT NULL,
-    Credit DECIMAL(11,2),
-    Debit DECIMAL(11,2),
-    Note VARCHAR(255),
-    PRIMARY KEY (Invoice, Note),
-    FOREIGN KEY (Invoice) REFERENCES sales(Invoice)
+CREATE TABLE sales_ledger (
+    invoice VARCHAR(255) NOT NULL,
+    credit DECIMAL(11,2),
+    debit DECIMAL(11,2),
+    note VARCHAR(255),
+    PRIMARY KEY (invoice, Note),
+    FOREIGN KEY (invoice) REFERENCES sales(invoice)
 );
 
-CREATE USER 'airbyte'@'%' IDENTIFIED BY 'airbyte';
-GRANT RELOAD, REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'airbyte'@'%';
-GRANT ALL PRIVILEGES ON oaken.* TO 'airbyte'@'%';
-FLUSH PRIVILEGES;
+CREATE ROLE airbyte WITH LOGIN PASSWORD 'airbyte';
+GRANT ALL PRIVILEGES ON DATABASE oaken TO airbyte;
+
+INSERT INTO positions VALUES (100, 'CEO');
+INSERT INTO positions VALUES (110, 'Vice President');
+INSERT INTO positions VALUES (120, 'Regional Manager');
+INSERT INTO positions VALUES (130, 'Branch Manager');
+INSERT INTO positions VALUES (140, 'Secretary');
+INSERT INTO positions VALUES (150, 'Sales Representative');
+INSERT INTO positions VALUES (160, 'Accountant');
+INSERT INTO positions VALUES (170, 'Inventory Specialist');
+INSERT INTO positions VALUES (180, 'Human Resource Specialist');
+INSERT INTO positions VALUES (190, 'Customer Service Representative');
+
+INSERT INTO employees (employee_id, first_name, last_name, title, manager_id, active)
+    VALUES (10000,'Alan','Brand',100,NULL,true);
+INSERT INTO employees (employee_id, first_name, last_name, title, manager_id, active)
+    VALUES (10001,'Jan','Levinson-Gould',110,10000,true);
+INSERT INTO employees (employee_id, first_name, last_name, title, manager_id, active)
+    VALUES (10002,'Nellie','Bertram',120,10001,true);
+INSERT INTO employees (employee_id, first_name, last_name, title, manager_id, active)
+    VALUES (10003,'Michael','Scott',130,10002,true);
+INSERT INTO employees (employee_id, first_name, last_name, title, manager_id, active)
+    VALUES (10004,'Pam','Beesly',140,10003,true);
+INSERT INTO employees (employee_id, first_name, last_name, title, manager_id, active)
+    VALUES (10005,'Oscar','Nunez',160,10002,true);
+INSERT INTO employees (employee_id, first_name, last_name, title, manager_id, active)
+    VALUES (10006,'Mindy','Kaling',190,10003,true);
+INSERT INTO employees (employee_id, first_name, last_name, title, manager_id, active)
+    VALUES (10007,'Tolby','Flenderson',180,10002,true);
+INSERT INTO employees (employee_id, first_name, last_name, title, manager_id, active)
+    VALUES (10008,'Darryl','Philbin',170,10003,true);
+-- Sales
+INSERT INTO employees (employee_id, first_name, last_name, title, manager_id, active)
+    VALUES (20001,'Dwight','Schrute',150,10003,true);
+INSERT INTO employees (employee_id, first_name, last_name, title, manager_id, active)
+    VALUES (20002,'Jim','Halpert',150,10003,true);
+INSERT INTO employees (employee_id, first_name, last_name, title, manager_id, active)
+    VALUES (20003,'Stanley','Hudson',150,10003,true);
+INSERT INTO employees (employee_id, first_name, last_name, title, manager_id, active)
+    VALUES (20004,'Phyllis','Vance',150,10003,true);
+INSERT INTO employees (employee_id, first_name, last_name, title, manager_id, active)
+    VALUES (20005,'Andy','Bernard',150,10003,true);
+INSERT INTO employees (employee_id, first_name, last_name, title, manager_id, active)
+    VALUES (20006,'Danny','Cordray',150,10003,true);
+INSERT INTO employees (employee_id, first_name, last_name, title, manager_id, active)
+    VALUES (20007,'Todd','Packer',150,10003,true);
+INSERT INTO employees (employee_id, first_name, last_name, title, manager_id, active)
+    VALUES (20008,'Karen','Filippell',150,10003,true);
